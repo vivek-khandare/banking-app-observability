62c01cf409fd5000ae629666f7ec0e0e4cc61223
vivek-khandare <khandarevivek78@gmail.com>
2026-06-28 16:52:34 +0530

    add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,33 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "banking-app-observability"
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "key_pair_name" {
+  description = "AWS EC2 Key Pair Name"
+  type        = string
+}
+
+variable "allowed_ip" {
+  description = "Your Public IP with /32 (Example: 103.xxx.xxx.xxx/32)"
+  type        = string
+}
