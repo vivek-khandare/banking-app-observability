2b5d58b26da8fc3a36ed270ff6e12e4efd17353e
vivek-khandare <khandarevivek78@gmail.com>
2026-06-29 15:56:41 +0530

    add some resources in ec2.tf and create outputs.tf file

---
+++ terraform/ec2.tf
@@ -0,0 +1,78 @@
+resource "aws_instance" "jenkins" {
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  associate_public_ip_address = true
+
+  user_data = file("${path.module}/userdata/jenkins-userdata.sh")
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-jenkins"
+    }
+  )
+}
+
+resource "aws_instance" "k3s_server" {
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+
+  associate_public_ip_address = true
+
+  user_data = file("${path.module}/userdata/k3s-server-userdata.sh")
+
+  root_block_device {
+    volume_size          = 20
+    volume_type          = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-k3s-server"
+    }
+  )
+}
+
+resource "aws_instance" "k3s_worker" {
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+
+  associate_public_ip_address = true
+
+  user_data = file("${path.module}/userdata/k3s-worker-userdata.sh")
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-k3s-worker"
+    }
+  )
+
+  depends_on = [
+    aws_instance.k3s_server
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "jenkins_public_ip" {
+  description = "Public IP of Jenkins Server"
+  value       = aws_instance.jenkins.public_ip
+}
 
+output "k3s_server_public_ip" {
+  description = "Public IP of K3s Server"
+  value       = aws_instance.k3s_server.public_ip
+}
+
+output "k3s_worker_public_ip" {
+  description = "Public IP of K3s Worker"
+  value       = aws_instance.k3s_worker.public_ip
+}
