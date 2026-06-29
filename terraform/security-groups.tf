333ec708939bbe84cfd4c50c712109f34780aa35
vivek-khandare <khandarevivek78@gmail.com>
2026-06-29 11:13:43 +0530

    Add Jenkins and K3s security groups

---
+++ terraform/security-groups.tf
@@ -0,0 +1,91 @@
+# -----------------------------------
+# Jenkins Security Group
+# -----------------------------------
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "${var.project_name}-jenkins-sg"
+  description = "Security Group for Jenkins Server"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-jenkins-sg"
+  }
+}
+
+# -----------------------------------
+# K3s Security Group
+# -----------------------------------
+
+resource "aws_security_group" "k3s_sg" {
+  name        = "${var.project_name}-k3s-sg"
+  description = "Security Group for K3s Server"
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  # Kubernetes API
+  ingress {
+    description = "K3s API Server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  # HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-k3s-sg"
+  }
+}
